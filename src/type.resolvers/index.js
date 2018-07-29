f41f85fe1caf4bf8678be306016890212a40e3f1
threem0126 <89479759@qq.com>
2018-07-30 00:18:48 +0800

    GraphqlType的自定义解析器

--- src/type.resolvers/index.js
+++ src/type.resolvers/index.js
@@ -1,6 +1,5 @@
 /**
- * GraphqlType的自定义解析器，在Prisma Graphql Schema后面执行
- * @type {{User: {name2: (function(*))}}}
+ * GraphqlType的自定义解析器，在Prisma Graphql Schema后面执行，可用来做自定义属性扩增、校验、修饰等
  */
 import User from './User'
 
